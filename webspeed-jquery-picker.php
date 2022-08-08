0f3afbd59ac89dbb3e98ccba2cedde486918d59d
Morten Andersen <morten@hjemmesider.dk>
2022-08-08 13:42:58 +0200

    Frist
    First time

---
+++ webspeed-jquery-picker.php
@@ -0,0 +1,20 @@
+<?php
+/*
+Plugin Name: Webspeed jQuery picker
+Version: 1.0
+Plugin URI: https://www.web.dk
+Description: Add jQuery Date picker to WordPress
+Author: Morten Andersen
+Text Domain: websepeed-picker-domain
+Author URI: https://www.web.dk.dk
+*/
+
+function webspeed_jquery_enqueue_datepicker() {
+    // Load the datepicker script (pre-registered in WordPress).
+    wp_enqueue_script( 'jquery-ui-datepicker' );
+
+    // You need styling for the datepicker. For simplicity I've linked to the jQuery UI CSS on a CDN.
+    wp_register_style( 'jquery-ui', 'https://code.jquery.com/ui/1.12.1/themes/smoothness/jquery-ui.css' );
+    wp_enqueue_style( 'jquery-ui' );  
+}
+add_action( 'wp_enqueue_scripts', 'webspeed_jquery_enqueue_datepicker' );
